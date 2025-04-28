8de4bfb417928cb54afdd8b0a1fef806be9369e8
Martin <martin_gomez_1996@hotmail.com>
2025-04-28 17:18:41 -0300

    Realizo actividad 5.1 'Listas'

---
+++ 05.1 Listas/Practico5.1.py
@@ -0,0 +1,60 @@
+# Actividad 1
+numeros = list(range(4,101,4))
+print(numeros)
+
+print("//////////////////////")
+# Actividad 2
+actividad2 = [0, 1, 2, 3, 4]
+print(actividad2[3])
+
+print("//////////////////////")
+# Actividad 3
+actividad3 = []
+actividad3.append("Hola")
+actividad3.append("como")
+actividad3.append("estas?")
+print(actividad3)
+
+print("//////////////////////")
+# Actividad 4
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[3] = "oso"
+print(animales)
+
+# Actividad 5
+#Primero se crea un array "numeros" con los siguientes elementos: 8, 15, 3, 22, 7
+#Se llama a remove y se elimina el elemento de mayor valor dentro del array (22)
+#Se imprime nuevamente la lista
+print("//////////////////////")
+
+# Actividad 6
+actividad6 = list(range(10, 31, 5))
+print(actividad6[:2])
+print("//////////////////////")
+
+# Actividad 7
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1:3] = ["auto", "familiar"]
+print(autos)
+print("//////////////////////")
+
+# Actividad 8
+dobles = []
+dobles.append(2*5)
+dobles.append(2*10)
+dobles.append(2*15)
+print(dobles)
+print("//////////////////////")
+
+# Actividad 9
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
+print("//////////////////////")
+
+# Actividad 10
+lista_anidada = [[15], [True], [[25.5],[57.9],[30.6]],[False]]
+print(lista_anidada)
